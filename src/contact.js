fa6ad2f34d5603f10ffcdc045b486049be34caa0
Kevin Mora <kambetacode@gmail.com>
2023-11-21 18:53:37 -0600

    feat: added new logic on contact.js added new logic to create new div to show contact information

--- src/contact.js
+++ src/contact.js
@@ -3,6 +3,7 @@ const contactPage = (container) => {
     const contact = document.createElement('div')
     let hero;
     let heroTitle;
+    let contactDiv;
 
     const createHero = () => {
         hero = document.createElement('div')
@@ -11,8 +12,20 @@ const contactPage = (container) => {
         heroTitle = document.createElement('h1')
         heroTitle.textContent = 'Contact us'
 
+        contactDiv = document.createElement('div')
+        contactDiv.classList.add('contact-div')
+        contactDiv.innerHTML = `
+            <p>P Sherman, 42 Wallaby Way, Sydney</p>
+            <p>555-555-5555</p>
+            <p>Tuesday - Sunday</p>
+            <p>11PM - 2AM</p>
+        `
+
+        
+
         hero.appendChild(heroTitle)
         contact.appendChild(hero)
+        contact.appendChild(contactDiv)
     }
 
     createHero()
